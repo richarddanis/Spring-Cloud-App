db214c8a0cc5570d63790f65e7de44ffda9b36cc
Richard Danis <Richard_Danis@epam.com>
2020-04-29 13:23:33 +0200

    Init commit

---
+++ guest-service/src/test/java/com/richard/danis/www/guest/StarterTests.java
@@ -0,0 +1,13 @@
+package com.richard.danis.www.guest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
