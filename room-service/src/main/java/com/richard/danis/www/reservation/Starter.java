890543566d52aae13e71632201fcc1c2997439b1
Richard Danis <Richard_Danis@epam.com>
2020-04-30 09:08:52 +0200

    add service discovery to another app

--- room-service/src/main/java/com/richard/danis/www/reservation/Starter.java
+++ room-service/src/main/java/com/richard/danis/www/reservation/Starter.java
@@ -5,12 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.core.io.DefaultResourceLoader;
 
 /**
  * Entry point of the application.
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Starter extends SpringBootServletInitializer {
 
     @Override
